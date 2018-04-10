b3bfc528effea5776ce27d43d9c8cfac9f41442a
Rouz Majlessi <rmajlessi@gmail.com>
2018-04-10 15:32:37 -0700

    Add OnModeCreating Handler in DbContext class

--- ProductCatalogApi/Data/CatalogContext.cs
+++ ProductCatalogApi/Data/CatalogContext.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductCatalogApi.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProductCatalogApi.Data
 {
-    public class CatalogContext
+    public class CatalogContext : DbContext
     {
+        public CatalogContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Handler method OnModelCreating
+        //Override object use ModeBuilder as parameter
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CatalogBrand>(ConfigureCatalogBrand);
+            builder.Entity<CatalogType>(ConfigureCatalogType);
+            builder.Entity<CatalogItem>(ConfigureCatalogItem);
+        }
+
+        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureCatalogBrand(EntityTypeBuilder<CatalogBrand> obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
